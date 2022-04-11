b50aaeaaa13adbd3d16dfa02488934c8fa510c85
Yury Gurevich <yury@gurevich.com.au>
2022-04-11 11:17:09 +1000

    progress

--- app.py
+++ app.py
@@ -12,6 +12,11 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/user/name")
+def user(name):
+    return render_template("user.html", user=name)
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
