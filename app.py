6728cf804f61584b311b9d7ec3805be8b88efd90
Yury Gurevich <yury@gurevich.com.au>
2022-04-11 16:59:59 +1000

    progress

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 # /usr/bin/python3
 
+from crypt import methods
 from ssl import HAS_TLSv1_1
 
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "my secret key"
+
+
+class NameForm(FlaskForm):
+    name = StringField("What is your name: ", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 
 @app.route("/")
@@ -12,9 +23,21 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/user/name")
+@app.route("/user/<name>")
 def user(name):
-    return render_template("user.html", user=name)
+    return render_template("user.html", name=name)
+
+
+@app.route("/name", methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NameForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+
+    return render_template("name.html", name=name, form=form)
 
 
 @app.errorhandler(404)
