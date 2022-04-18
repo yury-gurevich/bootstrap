98a15c3fcb0e1e2ec2408d8603257025a131c487
Yury Gurevich <yury@gurevich.com.au>
2022-04-18 11:47:49 +1000

    progress

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+# app.config['SESSION_COOKIE_SECURE'] = True
 
 # Initialise Database
 db = SQLAlchemy(app)
